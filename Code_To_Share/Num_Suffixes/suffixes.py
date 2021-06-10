4720176fe0f14095fb82f88cb31390f26aa2a0e1
Andre <astevens009@users.noreply.github.com>
2021-06-10 19:11:09 -0400

    Remove extraneous test for the 10's digit

--- Code_To_Share/Num_Suffixes/suffixes.py
+++ Code_To_Share/Num_Suffixes/suffixes.py
@@ -1,11 +1,11 @@
 def get_suffix(num):
     if num >= 11 and num < 20:
         return "th"
-    elif num == 1 or num % 10 == 1:
+    elif num % 10 == 1:
         return "st"
-    elif num == 2 or num % 10 == 2:
+    elif num % 10 == 2:
         return "nd"
-    elif num == 3 or num % 10 == 3:
+    elif num % 10 == 3:
         return "rd"
     else:
         return "th"
